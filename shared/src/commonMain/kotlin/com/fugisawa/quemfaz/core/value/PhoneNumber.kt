d795e019d8516a8d6f5f0f0a0c3e767409f62488
Lucas Fugisawa <lucasfugisawa@gmail.com>
2026-03-10 08:50:13 -0300

    Remove `isLikelyBrazilianPhoneNumber` method from `PhoneNumber` value class

--- shared/src/commonMain/kotlin/com/fugisawa/quemfaz/core/value/PhoneNumber.kt
+++ shared/src/commonMain/kotlin/com/fugisawa/quemfaz/core/value/PhoneNumber.kt
@@ -5,11 +5,4 @@ import kotlin.jvm.JvmInline
 
 @Serializable
 @JvmInline
-value class PhoneNumber(val value: String) {
-    fun isLikelyBrazilianPhoneNumber(): Boolean {
-        // Very conservative MVP check: at least 10 digits (DDD + number)
-        // Brazilian numbers are typically 55 (country) + 2 digits (DDD) + 8 or 9 digits
-        val digits = value.filter { it.isDigit() }
-        return digits.length >= 10
-    }
-}
+value class PhoneNumber(val value: String)
